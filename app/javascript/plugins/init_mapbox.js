6db7d94a004f0935370ca74edeab130de66efa42
stephanie rousset <strabon@club-internet.fr>
2020-09-14 18:59:15 +0200

    add marker to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,6 +10,20 @@ const initMapbox = () => {
     center: [2.35183, 48.85658],
     zoom: 12
     });
+
+     // create the popup
+    const popup = new mapboxgl.Popup({ offset: 25 })
+    .setHTML("<h1>Le bar du coin</h1><p>16 villa Gaudelet 75011 Paris</p>")
+    .setMaxWidth("300px");
+     // create DOM element for the marker
+    const el = document.createElement('div');
+    el.id = 'marker';
+
+    new mapboxgl.Marker(el)
+    .setLngLat([ 2.379734, 48.865425 ])
+    .setPopup(popup)  // sets a popup on this marker
+    .addTo(map);
   };
 };
 export { initMapbox };
+
