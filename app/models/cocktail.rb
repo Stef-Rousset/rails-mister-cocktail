9fc1840d29633865eeb1c924aefcf5f6b5c2147f
stephanie rousset <strabon@club-internet.fr>
2020-09-16 17:35:53 +0200

    add a search bar to home page

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,14 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: :name,
+    associated_against: {
+      ingredients: :name,
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
