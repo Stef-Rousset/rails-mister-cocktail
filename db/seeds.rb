4ff08a6296065f3b3904d178150c13d743679d8b
stephanie rousset <strabon@club-internet.fr>
2020-09-18 11:54:13 +0200

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,37 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
+puts 'destroy cocktails'
+Cocktail.destroy_all
 puts 'destroy ingredients'
 Ingredient.destroy_all
 
+
 puts 'creating ingredients'
-ingredients = ['lemon', 'ice', 'mint leaves', 'vodka', 'rhum', 'orange juice', 'coca', 'gin']
+ingredients = ["citron", "glace pilée", "glaçons", "feuilles de menthe", "vodka", "rhum", "jus d'orange", "jus d'ananas", "coca", "gin", "tequila", "eau gazeuse", "sirop de canne", "cachaça", "triple sec", "lait de coco"]
 
 ingredients.each do |ingredient|
   Ingredient.create!(name: ingredient)
 end
 puts 'done'
 
-puts 'destroy cocktails'
-Cocktail.destroy_all
+puts 'creating cocktails'
 
 margarita = Cocktail.create(name: 'margarita')
-file = URI.open('https://images.unsplash.com/photo-1556855810-ac404aa91e85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+file = URI.open('https://images.unsplash.com/photo-1590701833281-e6283af0948d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1825&q=80')
 margarita.photo.attach(io: file, filename: 'margarita.png', content_type: 'image/png')
 
-rhum = Cocktail.create(name: 'rhum')
-file_two = URI.open('https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1886&q=80')
-rhum.photo.attach(io: file_two, filename: 'rhum.png',content_type: 'image/png')
+mojito = Cocktail.create(name: 'mojito')
+file_two = URI.open('https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+mojito.photo.attach(io: file_two, filename: 'mojito.png',content_type: 'image/png')
 
-gin = Cocktail.create(name: 'gin')
-file_three = URI.open('https://images.unsplash.com/photo-1546171753-97d7676e4602?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
-gin.photo.attach(io: file_three, filename: 'gin.png',content_type: 'image/png')
+caipirinha = Cocktail.create(name: 'caipirinha')
+file_three = URI.open('https://images.unsplash.com/photo-1582568822042-613147a60a1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+caipirinha.photo.attach(io: file_three, filename: 'caipirinha.png',content_type: 'image/png')
+
+daiquiri = Cocktail.create(name: 'daiquiri')
+file_three = URI.open('https://images.unsplash.com/photo-1527762055594-4956c0c8c617?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80')
+daiquiri.photo.attach(io: file_three, filename: 'daiquiri.png',content_type: 'image/png')
 
 puts 'done'
 
